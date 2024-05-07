c98f864ff32fbbfa71706045f3a0581a12535626
2acholsk1 <p.zacholski@wp.pl>
2024-05-07 15:49:15 +0000

    init commit

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-# syntax=docker/dockerfile:1
-FROM osrf/ros2:humble
-
-RUN apt-get update -y -q
-
-WORKDIR /app
---
+++ prm_star_project/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'prm_star_project'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='2acholsk1',
+    maintainer_email='p.zacholski@wp.pl',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
